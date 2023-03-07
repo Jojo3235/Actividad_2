3b0642bd38ffe618a111fbe2a0cd1ddc1751c790
Jojo3235 <joralmand@gmail.com>
2023-03-08 00:53:58 +0100

    boceto del resto de ejercicios y ruta de que hacer

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,3 +3,10 @@
 lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
 
 lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']"""
+
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+
+lista_3 = list(set(lista_1)&set(lista_2))       # A partir de dos inputs separarlo '' y luego compararlos
+
+print(lista_3)
