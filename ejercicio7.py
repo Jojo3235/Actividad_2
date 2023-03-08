256c8718a17a9ee2eaa5aae5fa236cab02ef8ae3
Jojo3235 <joralmand@gmail.com>
2023-03-08 20:32:00 +0100

    ej7 done

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,23 +1,30 @@
-"""Ejercicio 7
-Realiza una función llamada agregar_una_vez(lista, el) que reciba una lista y un elemento. La función debe añadir el elemento al final de la lista con la condición de no repetir ningún elemento. Además si este elemento ya se encuentra en la lista se debe invocar un error de tipo ValueError que debes capturar y mostrar este mensaje en su lugar:
+class ControlDeListas:
 
-Error: Imposible añadir elementos duplicados => [elemento].
-Cuando tengas la función intenta añadir los siguiente valores a la lista 10, -2, "Hola" y luego muestra su contenido.
+    def __init__(self, lista, elemento):
+        self.lista = lista
+        self.elemento = elemento
 
-Sugerencia
+    @staticmethod
+    def comprobar_no_pertenencia(lista, elemento):
+        if elemento in lista:
+            return False
+        else:
+            return True
 
-Puedes utilizar la sintaxis "elemento in lista"
-
-elementos = [1, 5, -2]
-"""
-#sencillo, faltaria buscar como hacerlo en una clase, ya mañana
-
-def agregar_una_vez(lista, el):
-    #Quiza meter un while true para que no se cierre el programa si se introduce un valor erroneo, cambiar un poco de codigo ***
-    if el in lista:
-        raise ValueError("Error: Imposible añadir elementos duplicados => [elemento].") #***
-    else:
-        lista.append(el)
-        return lista    #Esto quiza ya meterlo en el __str__
+    @staticmethod
+    def append_lista(lista, elemento):
+        if ControlDeListas.comprobar_no_pertenencia(lista,elemento) == True:
+            lista.append(elemento)
+            return lista
+        else:
+            return "Error: Imposible añadir elementos duplicados => {}.".format(elemento)
+        
     
-#Faltan implementar tests y menu
+def main():
+    lista = [1,2,3,4,5]
+    elemento = 5
+    lista = ControlDeListas.append_lista(lista, elemento)
+    print(lista)
+
+if __name__ == "__main__":
+    main()
