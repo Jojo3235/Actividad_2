b75a16a4c3d400a158f68486ade3290a139030ae
Jojo3235 <joralmand@gmail.com>
2023-03-08 20:51:03 +0100

    eliminacion enunciado ej 6

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,25 +1,3 @@
-"""Ejercicio 6
-Realiza una función separar(lista) que tome una lista de números enteros y devuelva dos listas ordenadas. La primera con los números pares y la segunda con los números impares.
-
-Por ejemplo:
-
-pares, impares = separar([6,5,2,1,7])
-
-print(pares)
-
-print(impares)
-
-[2, 6]
-
-[1, 5, 7]
-
-Sugerencia
-
-Para ordenar una lista automáticamente puedes utilizar el método .sort()."""
-
-# Generar la clase donde se usara un metodo estatico para generar la separacion de la lista segun sean pares o impares, finalmente un metodo str que devuelva ambas listas, a lo mejor 
-# iniciarlas al principio de la clase para mayor comodidad
-
 class SepararLista:
     def __init__(self, lista):
         self.lista = lista
