05698f89b4d91bcad8c63d83546d83ce249ee8e4
Jojo3235 <joralmand@gmail.com>
2023-03-08 18:05:09 +0100

    ejercicio 3 done

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,6 +7,31 @@ lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']"""
 lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
 lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
 
-lista_3 = list(set(lista_1)&set(lista_2))       # A partir de dos inputs separarlo '' y luego compararlos
 
-print(lista_3)
+class ListasComparar:
+
+    def __init__(self, lista_1, lista_2):
+        self.lista_1 = lista_1
+        self.lista_2 = lista_2
+
+    @staticmethod
+    def generar_lista(*args):
+        lista = []
+        for arguments in args:
+            lista.append(arguments)
+        return lista
+    
+    @staticmethod
+    def comparar_listas(lista1, lista2):
+        return list(set(lista1)&set(lista2))
+    
+    def __str__(self):
+        return "Los elementos en común entre ambas listas son: {}".format(self.comparar_listas(self.lista_1, self.lista_2))
+    
+
+def main():
+    lista_compartida = ListasComparar(lista_1, lista_2)
+    print(lista_compartida)
+
+if __name__ == "__main__":
+    main()
