12bb2a46e05181976ad80df45344c6753296c7cb
Jojo3235 <joralmand@gmail.com>
2023-03-08 19:29:21 +0100

    descomposicion funcionando

--- descomposicion.py
+++ descomposicion.py
@@ -21,4 +21,38 @@ Pista
 
 Que el valor sea un número no significa necesariamente que deba serlo para formatearlo. Necesitarás jugar muy bien con los índices y realizar muchas conversiones entre tipos cadenas, números y viceversa"""
 
-# Crear un metodo de separacion de digitos, un metodo para formatearlos y un metodo para imprimirlos... pensar más mañana 
+# Crear un metodo de separacion de digitos, un metodo para formatearlos y un metodo para imprimirlos
+from helpers import pedir_numero
+
+class Descomposicion:
+    def __init__(self, numero):
+        self.numero = numero
+
+    @staticmethod
+    def descomponer_a_lista(numero):
+        descomposicion = []
+        while numero > 0:
+            descomposicion.append(numero % 10)
+            numero = numero // 10
+        return descomposicion
+
+    @staticmethod
+    def formatear(descomposicion):
+        for i in range(len(descomposicion)):
+            descomposicion[i] = str(descomposicion[i]*10**i).zfill(len(descomposicion))
+        return descomposicion[::-1]
+    
+    @staticmethod 
+    def imprimir(descomposicion):
+        for i in descomposicion:
+            print(i)
+
+def main():
+    numero = pedir_numero()
+    descomposicion = Descomposicion.descomponer_a_lista(numero)
+    descomposicion = Descomposicion.formatear(descomposicion)
+    Descomposicion.imprimir(descomposicion)
+
+
+if __name__ == "__main__":
+    main()
--- helpers.py
+++ helpers.py
@@ -7,3 +7,12 @@ def pedir_numero1_9(mensaje):
         except ValueError:
             pass
             print("El numero debe estar entre 1 y 9")
+
+def pedir_numero():
+    while True:
+        try:
+            numero = int(input("Introduce un numero: "))
+            return numero
+        except ValueError:
+            pass
+            print("El numero debe ser un entero")
